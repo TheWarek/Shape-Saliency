c7416c21769de278e5e755e9f35d72dce99cb0a0
Marek <marko.jakab@gmail.com>
2018-07-12 21:19:28 +0200

    [Impl] dataset creation
    h5py dataset
    local and global context

---
+++ utils/extract_dataset.py
@@ -0,0 +1,187 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2 as cv
+import numpy as np
+
+import sys, getopt, os
+import math
+from scipy.spatial import distance
+import random
+from sklearn import preprocessing
+import h5py
+
+
+from utils.util import *
+
+# area in px as borders for each shape
+area = 80
+image_size = 400
+verbose = 1 # Store images for view
+store_verbose = '../images/dataset/'
+
+# where to store h5py file
+store_file = '../datasets/data.h5'
+train_size = 0.8
+
+## INFO: Script to extract all shapes and their respective saliency from each model and store it for NN learning
+def extract_dataset():
+
+    # Get info about models
+    loc_models, glob_models = get_loc_glob_models_list()
+
+    # Prepare h5py format for storage
+    # Open file and prepare for append
+    h5f = h5py.File(store_file, 'a')
+    train_loc = h5f.create_dataset("train_loc", (0, image_size, image_size, len(loc_models) + 1),
+                                   maxshape=(None, image_size, image_size, len(loc_models) + 1))
+    valid_loc = h5f.create_dataset("valid_loc", (0, image_size, image_size, len(loc_models) + 1),
+                                   maxshape=(None, image_size, image_size, len(loc_models) + 1))
+    train_gt_loc = h5f.create_dataset("train_gt_loc", (0, image_size, image_size, 1),
+                                   maxshape=(None, image_size, image_size, 1))
+    valid_gt_loc = h5f.create_dataset("valid_gt_loc", (0, image_size, image_size, 1),
+                                      maxshape=(None, image_size, image_size, 1))
+
+    train_glob = h5f.create_dataset("train_glob", (0, image_size, image_size, len(glob_models) + 1),
+                                   maxshape=(None, image_size, image_size, len(glob_models) + 1))
+    valid_glob = h5f.create_dataset("valid_glob", (0, image_size, image_size, len(glob_models) + 1),
+                                   maxshape=(None, image_size, image_size, len(glob_models) + 1))
+    train_gt_glob = h5f.create_dataset("train_gt_glob", (0, image_size, image_size, 1),
+                                      maxshape=(None, image_size, image_size, 1))
+    valid_gt_glob = h5f.create_dataset("valid_gt_glob", (0, image_size, image_size, 1),
+                                      maxshape=(None, image_size, image_size, 1))
+
+    # Get list of images used in experiment
+    images = get_images_list()
+
+    # List of images to store later:
+    store_image_loc = []
+    store_image_glob = []
+    store_gt_loc = []
+    store_gt_glob = []
+
+    # Extract basic shape image and its coordinates
+    for i, image in enumerate(images):
+        # For each contour:
+        # 1. get coordinates
+        # 2. store unedited shape
+        # 3. store all local models
+        # 4. store ground truth
+
+        # Load image and binarize it
+        img = cv.imread(os.path.join('../images/.', image), 0)
+        name = os.path.splitext(image)[0]
+
+        # binarize it (just to be sure)
+        ret, thr = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+        # find contours of image
+        # cv.CHAIN_APPROX_NONE -> we are looking for each boundary pixel
+        # cv.RETR_EXTERNAL -> we are not looking for contours in contours
+        im2, contours, hierarchy = cv.findContours(thr, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+        # Ground truth for global saliency models # We can just compute it once
+        blank_image = np.zeros((thr.shape[0], thr.shape[1], 1), np.uint8)
+        gt_glob_image, scaled_gt_glob_image = get_global_gt(name, blank_image, max_fix=4, sigma=32, kernel=81,
+                                                            binary=False)
+
+        for j, cnt in enumerate(contours):
+            if cnt.shape[0] < 30:
+                # erase noise
+                continue
+            # For each contour store all information and resize it for NN
+            max_x, min_x, max_y, min_y = get_contour_margins(cnt)
+
+            bx, by, bw, bh = cv.boundingRect(cnt)
+
+            #### LOCAL CONTEXT ####
+
+            # area px on each side
+            blank_image = np.zeros((max_y - min_y + 2 * area, max_x - min_x + 2 * area, 1), np.uint8)
+            shape_image, scaled_shape_image = get_clipped_shape(thr, bx, by, bw, bh, area, image_size)
+            gt_loc_shape_image, scaled_gt_loc_shape_image = get_local_gt(name, blank_image, bx, by, bw, bh, max_fix=4, sigma=32,
+                                                             kernel=81, binary=False, area=area, resize=image_size)
+            # scaled image 0 - 1 for network.
+            result_image = scaled_shape_image
+            if verbose == 1:
+                cv.imwrite(store_verbose + str(i) + '_' + str(j) + '_gt_local.png', gt_loc_shape_image)
+
+            # extract each local model now:
+            for model in loc_models:
+                mod_img = cv.imread(os.path.join('../models/local/.', model, image), 0)
+                mod_shape, scaled_mod_shape = get_clipped_shape(mod_img, bx, by, bw, bh, area, image_size)
+                # Create multichannel image
+                result_image = np.concatenate((result_image, scaled_mod_shape), axis=2)
+                if verbose == 1:
+                    cv.imwrite(store_verbose+str(i)+'_'+str(j)+'_'+model+'.png',mod_shape)
+
+            store_image_loc.append(result_image)
+            store_gt_loc.append(scaled_gt_loc_shape_image)
+
+            #### GLOBAL CONTEXT ####
+
+            gt_glob_shape_image, scaled_gt_glob_shape_image = get_clipped_shape(gt_glob_image, bx, by, bw, bh, area, image_size)
+            result_image = scaled_shape_image
+            if verbose == 1:
+                cv.imwrite(store_verbose + str(i) + '_' + str(j) + '_gt_global.png', gt_glob_shape_image)
+
+            # extract each global model now:
+            for model in glob_models:
+                mod_img = cv.imread(os.path.join('../models/global/.', model, image), 0)
+                mod_shape, scaled_mod_shape = get_clipped_shape(mod_img, bx, by, bw, bh, area, image_size)
+                # Create multichannel image
+                result_image = np.concatenate((result_image, scaled_mod_shape), axis=2)
+                if verbose == 1:
+                    cv.imwrite(store_verbose+str(i)+'_'+str(j)+'_'+model+'.png',mod_shape)
+
+            store_image_glob.append(result_image)
+            store_gt_glob.append(scaled_gt_glob_shape_image)
+
+    # APPEND INTO H5PY FILE
+    # LOCAL
+    # Split into training / valid datasets
+    end_t = round(len(store_image_loc) * train_size)
+    train_set = store_image_loc[1:end_t, :, :, :]
+    valid_set = store_image_loc[end_t:, :, :, :]
+    train_gt_set = store_gt_loc[1:end_t, :, :, :]
+    valid_gt_set = store_gt_loc[end_t:, :, :, :]
+
+    # Store as an append file
+    # First reshape
+    train_loc.resize(train_loc.shape[0] + len(train_set), axis=0)
+    valid_loc.resize(valid_loc.shape[0] + len(valid_set), axis=0)
+
+    train_gt_loc.resize(train_gt_loc.shape[0] + len(train_gt_set), axis=0)
+    valid_gt_loc.resize(valid_gt_loc.shape[0] + len(valid_gt_set), axis=0)
+
+    # Now append
+    train_loc[-len(train_set):] = train_set
+    valid_loc[-len(valid_set):] = valid_set
+    train_gt_loc[-len(train_set):] = train_gt_set
+    valid_gt_loc[-len(valid_set):] = valid_gt_set
+
+    # GLOBAL
+    # Split into training / valid datasets
+    end_t = round(len(store_image_glob) * train_size)
+    train_set = store_image_glob[1:end_t, :, :, :]
+    valid_set = store_image_glob[end_t:, :, :, :]
+    train_gt_set = store_gt_glob[1:end_t, :, :, :]
+    valid_gt_set = store_gt_glob[end_t:, :, :, :]
+
+    # Store as an append file
+    # First reshape
+    train_glob.resize(train_glob.shape[0] + len(train_set), axis=0)
+    valid_glob.resize(valid_glob.shape[0] + len(valid_set), axis=0)
+
+    train_gt_glob.resize(train_gt_glob.shape[0] + len(train_gt_set), axis=0)
+    valid_gt_glob.resize(valid_gt_glob.shape[0] + len(valid_gt_set), axis=0)
+
+    # Now append
+    train_glob[-len(train_set):] = train_set
+    valid_glob[-len(valid_set):] = valid_set
+    train_gt_glob[-len(train_set):] = train_gt_set
+    valid_gt_glob[-len(valid_set):] = valid_gt_set
+
+
+if __name__ == "__main__":
+    extract_dataset()
---
+++ utils/util.py
@@ -0,0 +1,138 @@
+import cv2 as cv
+import numpy as np
+import sys, getopt, os
+import math
+
+
+def get_loc_glob_models_list():
+    # Get info about models
+
+    loc_models = []
+    glob_models = []
+
+    with open('../models/info.txt', 'r') as file:
+        line = True
+        while line:
+            line = file.readline()
+            if not line:
+                break
+            line_split = line.split()
+
+            # In case of new participant reset
+            if line_split[0] == 'local:':
+                loc_models.append(line_split[1])
+            if line_split[0] == 'global:':
+                glob_models.append(line_split[1])
+
+    return loc_models, glob_models
+
+def get_images_list():
+
+    images = []
+    for file in os.listdir('../images/.'):
+        if file.endswith(".png"):
+            name = os.path.splitext(file)[0]
+            images.append(name)
+
+    return images
+
+def get_contour_margins(cnt):
+    # find max / min point of the contour
+    max_x = np.max([a[0][0] for a in cnt])
+    min_x = np.min([a[0][0] for a in cnt])
+    max_y = np.max([a[0][1] for a in cnt])
+    min_y = np.min([a[0][1] for a in cnt])
+
+    return max_x, min_x, max_y, min_y
+
+def get_local_gt(name, blank_image, bx, by, bw, bh, max_fix, sigma, kernel, binary, area, resize):
+
+    # scaler = MinMaxScaler(copy=True, feature_range=(0, 255))
+    # scaler.fit()
+
+    with open('../fixations/' + name + '.txt', 'r') as file:
+        line = True
+        max_i = 0
+        while line:
+            line = file.readline()
+            if not line:
+                break
+            line_split = line.split()
+            # In case of new participant reset
+            if len(line_split) <= 1:
+                max_i = 0
+                continue
+            # in case of overcoming the number of fixation for current participant continue
+            if max_i >= max_fix:
+                continue
+            pt_x = float(line_split[3])
+            pt_y = float(line_split[4])
+
+            # In case of fixation within bounding area:
+            if (bx <= pt_x <= bx + bw) and (by <= pt_y <= by + bh):
+                max_i = max_i + 1
+                blank_image[int(math.floor(pt_y - by)) - 2 + area:int(math.floor(pt_y - by)) + 2 + area, int(math.floor(pt_x - bx)) - 2 + area:int(math.floor(pt_x - bx)) + 2 + area, 0] = 255
+                # cv.imshow('saliency', blank_image)
+                # cv.waitKey(0)
+                # cv.destroyAllWindows()
+
+    if resize > 0:
+        blank_image = cv.resize(blank_image, (resize, resize))
+
+    if sigma > 0:
+        blank_image = cv.GaussianBlur(blank_image, (int(kernel), int(kernel)), float(sigma))
+        #scale image up to 255
+        blank_image = blank_image * int(255 / blank_image.max())
+        # scale 0 - 1 = 0 - 255 for network
+        scaled_image = np.zeros((blank_image.shape[0], blank_image.shape[1], 1), np.float64)
+        delim = 1./255
+        scaled_image = blank_image * delim
+        #blank_image = scaler.transform(blank_image)
+    return blank_image, scaled_image
+
+def get_global_gt(name, blank_image, max_fix, sigma, kernel, binary):
+    with open('../fixations/' + name + '.txt', 'r') as file:
+        line = True
+        max_i = 0
+        while line:
+            line = file.readline()
+            if not line:
+                break
+            line_split = line.split()
+            # In case of new participant reset
+            if len(line_split) <= 1:
+                max_i = 0
+                continue
+            # in case of overcoming the number of fixation for current participant continue
+            if max_i >= max_fix:
+                continue
+            pt_x = float(line_split[3])
+            pt_y = float(line_split[4])
+
+            # In case of fixation:
+            max_i = max_i + 1
+            blank_image[int(math.floor(pt_y)) - 2:int(math.floor(pt_y)) + 2, int(math.floor(pt_x)) - 2:int(math.floor(pt_x)) + 2, 0] = 255
+
+    if sigma > 0:
+        blank_image = cv.GaussianBlur(blank_image, (int(kernel), int(kernel)), float(sigma))
+        #scale image up to 255
+        blank_image = blank_image * int(255 / blank_image.max())
+        # scale 0 - 1 = 0 - 255 for network
+        scaled_image = np.zeros((blank_image.shape[0], blank_image.shape[1], 1), np.float64)
+        delim = 1./255
+        scaled_image = blank_image * delim
+        #blank_image = scaler.transform(blank_image)
+    return blank_image, scaled_image
+
+def get_clipped_shape(from_image, bx, by, bw, bh, area, resize):
+    shape_image = np.zeros((bh + 2 * area, bw + 2 * area, 1), np.uint8)
+    shape_image[area:area + bh, area:area + bw, 0] = from_image[by:by + bh, bx:bx + bw]
+
+    if resize > 0:
+        shape_image = cv.resize(shape_image, (resize, resize))
+
+    # We need to scale it to 0-1 based on whole image (in case of global saliency models)
+    scaled_shape_image = np.zeros((shape_image.shape[0], shape_image.shape[1], 1), np.float64)
+    scaled_shape_image = shape_image * 1./255.
+
+    return shape_image, scaled_shape_image
